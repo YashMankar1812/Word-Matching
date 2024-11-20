9cb3dd84c0fc22064a31dd3cbf9ecd3d16579e14
Yash Mankar <160096931+YashMankar1812@users.noreply.github.com>
2024-11-20 14:02:15 +0530

    Added Work-Matching project

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const App = () => {
+
+
+  return (
+    <h1 className="text-3xl font-bold underline  text-blue-950">
+      Word Connect 
+    </h1>
+
+  );
+};
+
+export default App;
